a9c3a89a49470f2dbacfa8b44b806d19765b05da
Tibor Kiss <tibor.kiss01@sap.com>
2019-11-08 13:03:40 +0100

    Console Log

--- js/get_path.js
+++ js/get_path.js
@@ -1,8 +1,9 @@
-jQuery(document).ready(function( $ ){
-  var myUrl = window.location.href;
-  var myPage = window.location.pathname;
-  if (myPage == "/lifepath/"){
-    document.getElementById("demo").innerHTML = "The full URL of this page is:<br>" + myUrl;
-  }
-  document.alert("I am Tibi");
+jQuery(document).ready(function ($) {
+    console.log("Haho");
+    var myUrl = window.location.href;
+    var myPage = window.location.pathname;
+    if (myPage == "/lifepath/") {
+        document.getElementById("demo").innerHTML = "The full URL of this page is:<br>" + myUrl;
+    }
+    document.alert("I am Tibi");
 });
