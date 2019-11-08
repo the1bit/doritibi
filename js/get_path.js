8a4e47f4e563c6de0dfe0c3ddc76155e3e9e660a
Tibor Kiss <tibor.kiss01@sap.com>
2019-11-08 12:50:44 +0100

    get_path

---
+++ js/get_path.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function( $ ){
+  var myUrl = window.location.href;
+  var myPage = window.location.pathname;
+  if (myPage == "/lifepath/"){
+    document.getElementById("demo").innerHTML = "The full URL of this page is:<br>" + myUrl;
+  }
+  alert("I am Tibi");
+});
